f3bbbf3466a8c153590be39f6049569b7d3fa496
veroarriola <v.arriola@ciencias.unam.mx>
2024-12-14 09:30:43 -0600

    Arreglé error de cv2 sólo si no es Raspberry

--- i2c/webcami2c.py
+++ i2c/webcami2c.py
@@ -7,7 +7,7 @@
 
 IN_RASPBERRY = True
 
-import cv2
+
 import io
 import logging
 import socketserver
@@ -20,6 +20,7 @@ if IN_RASPBERRY:
     from picamera2.encoders import JpegEncoder
     from picamera2.outputs import FileOutput
 else:
+    import cv2
     from imutils.video import VideoStream
     import threading
 
